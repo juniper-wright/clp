39b1998c734e82afe8ba1a7aaa4148c8cb0ba647
phoenixfyre <ianufyrebird@gmail.com>
2013-10-18 17:07:52 -0400

    Added copyright on print
    Used @media print to show a copyright watermark when users try to print.

--- top.php
+++ top.php
@@ -14,8 +14,12 @@
 				document.getElementById('main').style.opacity = "1";
 			}
 		</script>
+		<script src="functions.js"></script>
+
+
 	</head>
 	<body style="background:#FFFDD0">
+		<div id="watermark"><img src="images/copyright.png" width="1024"></div>
 		<div id="main">
 			<div id="top" style="width:100%;">
 				<img src="images/logo.png" width="600" align="right"><div style="height:150px;">&nbsp;</div>
@@ -26,13 +30,15 @@
 				 </div>
 				 <div class="nav" onmouseover="shownav();" onmouseout="hidenav();" style="padding-top:20px; position: relative;">
 					<img src="images/C2.png" width="204">
-					 <div id="sub-nav" onmouseover="shownav();" onmouseout="hidenav();">
-						Families<br>
-						Glamour<br>
-						Weddings<br>
-						Events<br>
-						Personal Work
-					 </div>
+					<div id="sub-nav" onmouseover="shownav();" onmouseout="hidenav();">
+						<ul style="list-style:none; list-style-type:none; padding:0px;">
+							<li><a href="gallery.php?g=families">Families<a></li>
+							<li><a href="gallery.php?g=glamour">Glamour<a></li>
+							<li><a href="gallery.php?g=weddings">Weddings<a></li>
+							<li><a href="gallery.php?g=events">Events<a></li>
+							<li><a href="gallery.php?g=personal">Personal Work<a></li>
+						</ul>
+					</div>
 				 </div>
 				 <div class="nav">
 					<img src="images/C3.png" width="204">

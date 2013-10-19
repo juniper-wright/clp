2ff8056bf2510c67111b7971569b60635a3172a1
phoenixfyre <ianufyrebird@gmail.com>
2013-10-19 13:14:49 -0400

    Added license information

--- about.php
+++ about.php
@@ -1,3 +1,21 @@
+<?php
+	/*
+	This file is part of CLPPhotoSite.
+
+	CLPPhotoSite is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	CLPPhotoSite is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with CLPPhotoSite.  If not, see <http://www.gnu.org/licenses/>.
+	*/ 
+?>
 <?php include("top.php"); ?>
 	<div style="width:780px; height:520px; background:#D2D2D2; border: 3px solid #333333; float:left;">
 		<table width="780" cellpadding="10" style="font-family: Verdana, Arial; font-size:14px;">
--- contact.php
+++ contact.php
@@ -1,4 +1,23 @@
-<?php include("top.php"); ?>
+<?php
+	/*
+	This file is part of CLPPhotoSite.
+
+	CLPPhotoSite is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	CLPPhotoSite is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with CLPPhotoSite.  If not, see <http://www.gnu.org/licenses/>.
+	*/ 
+
+	include("top.php");
+?>
 	<div style="width:780px; height:520px; background:#D2D2D2; border: 3px solid #333333; float:left;">
 		<div style="margin:50px;">
 			<form name="contactform" method="post" action="send_form_email.php">

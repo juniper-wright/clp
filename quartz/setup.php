5e47c2aa6c30eb3846ed9dc0a299067d91e3816c
phoenixfyre <ianufyrebird@gmail.com>
2013-10-19 10:17:47 -0400

    Setup was not necessary at all.

--- quartz/setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$link = mysql_connect('LOCALHOST', 'clpmysqi1', 'CG0rkLOYY1Nr!');
-	mysql_select_db('clp', $link);
-	mysql_query("INSERT INTO administrators (admin_name, admin_pass) VALUES ('clm', '" . crypt('password', 'clmclpsalt') . "')");
-
-?>

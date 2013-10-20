066d79063a7d2cc7dd5fb6a96bae34123e1ee444
phoenixfyre <ianufyrebird@gmail.com>
2013-10-20 13:09:53 -0400

    Updated About and Contact pages

--- about.php
+++ about.php
@@ -17,16 +17,19 @@
 	*/ 
 ?>
 <?php include("top.php"); ?>
-	<div style="width:780px; height:520px; background:#D2D2D2; border: 3px solid #333333; float:left;">
-		<table width="780" cellpadding="10" style="font-family: Verdana, Arial; font-size:14px;">
+	<div style="padding:18px; background-color:#D2D2D2;">
+		<table width="740" cellpadding="10" style="font-family: Verdana, Arial; font-size:16px;">
 			<tr>
-				<td width="300">
-					<img src="images/about.jpg" width=300px height=415px><br/><center>Photo by Ceyonna Toler</center>
+				<td width="300" valign="top">
+					<img src="images/about.jpg" width=300px height=415px>
 				</td>
 				<td width="480" valign="top">
-					Chelsea Lyn began her life in Philadelphia, and instantly grew very fond of the city. She moved away with her family at a young age, and always yearned to return. In September 2009, shortly after her high school graduation, she did just that, to start her photography education. She attends Antonelli Institute of Art and Photography, located just outside of the Philadelphia city limits. In Spring 2011, she will graduate from Antonelli Institute with an Associate in Specialized Technology degree. 
-					<br />Chelsea became passionate about photography at a young age and quickly caught on that it was more than just a passion; it was embedded in her genes. At a very young age, Chelsea watched her dad always playing with his film SLR, and always loved being in front of the camera. At 13 years old, she knew that photography was what she wanted to do for the rest of her life. A few years later, Chelsea discovered that on her dad's side of the family, her great-grandfather owned his own darkroom and a studio in downtown Philadelphia.
-					<br />Chelsea loves photographing anything and everything, as long as she has her camera, named Roxanne, in her hands. She especially loves to photograph people, and she devotes herself to capturing a person's true beauty and emotion in each and every one of her photos.
+					<span style="line-height:20px;"><b>I'm Chelsea,</b>
+					<br>the owner of Chelsea Lyn Photography.</span>
+					<br>
+					<br>It took me until I was about 12 years old to like that middle name, so I'm making up for lost time!  I've always been passionate about photography; I was only 13 years old when I declared that I was going to be a great photographer.
+					<br><br>
+					I'm a very outgoing person and can make friends with anyone.  I love a child's carefree spirit and love photographing their natural spirit.  I love the late afternoon light - right before sunset.  Music has always played a big part in my life, and the 80s is definitely my favorite era of music.  I love eating nachos.  Summer is my favorite season because I love flip flops and putting my toes in the ocean.  The Eagles & Flyers will always have a big spot in my heart. Most importantly, I would love to update your family portrait.
 				</td>
 			</tr>
 		</table>
--- contact.php
+++ contact.php
@@ -18,63 +18,77 @@
 
 	include("top.php");
 ?>
-	<div style="width:780px; height:520px; background:#D2D2D2; border: 3px solid #333333; float:left;">
-		<div style="margin:50px;">
-			<form name="contactform" method="post" action="contact.php">
-				<table width="680" style="font-size: 14px; font-family: Verdana, Arial, Sans-serif;">
-					<tr>
-						<td valign="top">
-							<label for="first_name">First Name *</label>
-						</td>
-						<td valign="top">
-							<input  type="text" name="first_name" maxlength="50" size="30">
-						</td>
-						<td valign="center" align="left" rowspan="4">
-							<a href="http://www.facebook.com/chelsealynphotography"><img width="56px" height="57px" src="images/facebook_icon.png" style="border:none"></a>
-							<br>
-							Visit me on Facebook!
-						</td>
-					</tr>
-					<tr>
-						<td valign="top"">
-							<label for="last_name">Last Name *</label>
-						</td>
-						<td valign="top">
-							<input  type="text" name="last_name" maxlength="50" size="30">
-						</td>
-					</tr>
-					<tr>
-						<td valign="top">
-							<label for="email">Email Address *</label>
-						</td>
-						<td valign="top">
-							<input  type="text" name="email" maxlength="80" size="30">
-						</td>
-					</tr>
-					<tr>
-						<td valign="top">
-							<label for="telephone">Telephone Number</label>
-						</td>
-						<td valign="top">
-							<input  type="text" name="telephone" maxlength="30" size="30">
-						</td>
-					</tr>
-					<tr>
-						<td valign="top">
-							<label for="comments">Comments *</label>
-						</td>
-						<td valign="top" colspan="2">
-							<textarea  name="comments" maxlength="1000" cols="50" rows="6"></textarea>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2" style="text-align:center">
-							<input type="submit" value="Submit">
-							<input type="reset" value="Reset">
-						</td>
-					</tr>
-				</table>
-			</form>
-		</div>
+	<div style="padding:35px; padding-bottom: 46px; background-color:#D2D2D2; font-family: Verdana, Arial, Sans-serif; font-size:16px;">
+		Please use this fill out this form and tell me a little about your family or event, and I will get back to you within 48 hours.
+		<br>
+		<hr style="margin-top:15px;">
+		<form name="contactform" method="post" action="contact.php">
+			<table width="680" style="font-size: 14px; font-family: Verdana, Arial, Sans-serif;">
+				<tr>
+					<td valign="top">
+						<label for="first_name">First Name *</label>
+					</td>
+					<td valign="top">
+						<input  type="text" name="first_name" maxlength="50" size="30">
+					</td>
+					<td valign="center" align="left" rowspan="4" width="280">
+						<a href="http://www.facebook.com/chelsealynphotography" target="_blank"><img width="56" height="56" src="images/facebook_icon.png" style="border:none"></a>
+						<a href="//www.etsy.com/shop/ChelseaLynPhoto?ref=offsite_badges&utm_source=sellers&utm_medium=badges&utm_campaign=en_isell_1" target="_blank"><img width="56" height="56" src="images/etsy.png"></a>
+						<a href="http://chelsealynphoto.wordpress.com/" target="_blank"><img width="56" height="56" src="images/wordpress.png" style="border:none;"></a>
+					</td>
+				</tr>
+				<tr>
+					<td valign="top"">
+						<label for="last_name">Last Name *</label>
+					</td>
+					<td valign="top">
+						<input  type="text" name="last_name" maxlength="50" size="30">
+					</td>
+				</tr>
+				<tr>
+					<td valign="top">
+						<label for="email">Email Address *</label>
+					</td>
+					<td valign="top">
+						<input  type="text" name="email" maxlength="80" size="30">
+					</td>
+				</tr>
+				<tr>
+					<td valign="top">
+						<label for="telephone">Telephone Number</label>
+					</td>
+					<td valign="top">
+						<input  type="text" name="telephone" maxlength="30" size="30">
+					</td>
+				</tr>
+				<tr>
+					<td valign="top">
+						<label for="comments">Comments *</label>
+					</td>
+					<td valign="top" colspan="2">
+						<textarea  name="comments" maxlength="1000" cols="50" rows="6"></textarea>
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td colspan="2" align="left">
+						<input type="submit" value="Submit">
+					</td>
+				</tr>
+			</table>
+		</form>
+		<hr>
+		You can also contact me directly via:<br>
+		<table>
+			<tr>
+				<th width="150" align="left">Email:</th>
+				<td align="left"><a href="mailto:chelsea@chelsealynphotography.com">Chelsea@ChelseaLynPhotography.com</a></td>
+			</tr>
+			<tr>
+				<th width="150" align="left">Telephone:</th>
+				<td align="left">(267) 586-7254</td>
+			</tr>
+		</table>
+		Serving Philadelphia and surrounding areas.
 	</div>
 <?php include("bottom.php"); ?>

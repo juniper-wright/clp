5eeeb6bf12ea9734cd20450cc928fd47342d90e8
phoenixfyre <ianufyrebird@gmail.com>
2013-10-23 11:18:04 -0400

    About Update
    Resolves #12.

--- about.php
+++ about.php
@@ -22,18 +22,20 @@
 ?>
 	<style>.photoslider{background: #EEE;}</style>
 	<div style="padding:18px; font-family: Verdana, Arial, Sans-serif; font-size:16px;">
-		<table width="720" cellpadding="10" >
+		<table width="720">
 			<tr>
-				<td width="300" valign="top">
+				<td width="300" valign="middle">
 					<img src="images/about.jpg" width="300" height="415" style="box-shadow:0px 0px 0px 2px #333, 0px 0px 0px 6px #FFF, 0px 0px 0px 8px rgba(0,0,0,0.5);">
 				</td>
-				<td width="480" valign="top">
+				<td width="480" valign="top" style="font-size:14px; padding-left:20px;">
 					<span style="line-height:20px;"><b>I'm Chelsea,</b>
 					<br>the owner of Chelsea Lyn Photography.</span>
-					<br>
-					<br>It took me until I was about 12 years old to like that middle name, so I'm making up for lost time!  I've always been passionate about photography; I was only 13 years old when I declared that I was going to be a great photographer.
 					<br><br>
-					I'm a very outgoing person and can make friends with anyone.  I love a child's carefree spirit and love photographing their natural spirit.  I love the late afternoon light - right before sunset.  Music has always played a big part in my life, and the 80s is definitely my favorite era of music.  I love eating nachos.  Summer is my favorite season because I love flip flops and putting my toes in the ocean.  The Eagles & Flyers will always have a big spot in my heart. Most importantly, I would love to update your family portrait.
+					It took me until I was about 12 years old to like that middle name, so I'm making up for lost time! I've always been passionate about photography; I was only 13 years old when I declared that I was going to be a great photographer.
+					<br><br>
+					I graduated from Antonelli Institute of Art & Photography in 2011. I currently am a member and the director of communications for the Guild of Professional Photographers of the Delaware Valley. In Spring 2013, I entered the semi-annual print competition and won“Best of Weddings.”That is my first photography award, and my proudest moment yet!
+					<br><br>
+					I'm a very outgoing person and can make friends with anyone. I love a child's carefree spirit and love photographing their natural spirit. I love the late afternoon light - right before sunset. Music has always played a big part in my life, and the 80s is definitely my favorite era of music. I love eating nachos. Summer is my favorite season because I love flip flops and putting my toes in the ocean. The Eagles & Flyers will always have a big spot in my heart.
 				</td>
 			</tr>
 		</table>
